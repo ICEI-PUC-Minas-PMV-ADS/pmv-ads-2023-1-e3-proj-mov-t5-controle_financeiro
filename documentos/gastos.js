2cc330688c75befe3a59b40cbc084c58a07259c5
Gilvimar Vieira Perdigão <92383852+gilvimar-perdigao@users.noreply.github.com>
2023-06-04 19:32:56 -0300

    Create gastos.js

---
+++ documentos/gastos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Container from '../components/Container';
+import Body from '../components/Body';
+
+
+
+const Gastos = () =>{
+
+  return (
+    <Container>
+      < Header title={'Controle de Gastos'} />
+      <Body>
+      </Body>
+    </Container>
+  );
+
+
+
+}
+
+export default Gastos;
